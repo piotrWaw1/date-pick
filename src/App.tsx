4a807e99153a03f71a6cc2b34e696e9e6b6806b5
Piotr <piotrwc49@gmail.com>
2024-09-09 15:10:08 +0200

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import Header from "@/components/header/header.tsx";
 import SelectDates from "@/components/select-dates/select-dates.tsx";
 import Output from "@/components/output/output.tsx";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function App() {
   return (
     <>
       <Header/>
-      <div className="container m-auto grid md:grid-cols-3 gap-10 mt-5">
-        <div className=" p-3">
-          <SelectDates/>
+      <div className="container m-auto grid xl:grid-cols-3 gap-10 mt-5">
+        <div className="p-3 flex justify-center xl:col-span-1 col-span-2">
+          <Card>
+            <CardHeader>
+              <CardTitle>Wybierz zakres dat</CardTitle>
+              <CardDescription>Wypełnij cały formularz a dane zostaną wyświetlone w tabelce obok.</CardDescription>
+            </CardHeader>
+            <CardContent className="flex justify-center">
+              <SelectDates/>
+            </CardContent>
+          </Card>
         </div>
         <div className="p-3 col-span-2">
           <Output/>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,8 +2,8 @@ import { ModeToggle } from "@/components/mode-toggle.tsx";
 
 const Header = () => {
   return (
-    <header className="flex h-14">
-      <div className="container flex justify-between items-center m-auto">
+    <header className="container m-auto h-14">
+      <div className="flex justify-between items-center m-auto">
         <div>
           <h1 className="text-2xl font-bold">Date-pick</h1>
         </div>
